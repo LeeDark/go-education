e1b795dc92b1ee1b04a7147a9b50ba87514cf72c
LeeDark <sergey.povarnin@gmail.com>
2022-04-27 18:54:29 +0300

    Updated home endpoind. Added ping endpoint.

--- trainee-tester/test-http-server/main.go
+++ trainee-tester/test-http-server/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/ping", ping)
 
 	server := &http.Server{
 		Addr:           ":8090",
@@ -24,6 +25,17 @@ func main() {
 	server.ListenAndServe()
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+// Active endpoint = HTTP Server gives HTML page
+func home(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, `
+	<HTML>
+	<HEAD><TITLE>HTTP Server</TITLE></HEAD>
+	<BODY><P align='center'>Hello! Welcome to HTTP Server written on Golang!</P></BODY>
+	</HTML>
+	`)
+}
+
+// Passive endpoint = HTTP Server gives JSON (XML) data = Frontend uses this JSON (XML) data
+func ping(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "{ \"answer\": \"pong\"}")
 }
