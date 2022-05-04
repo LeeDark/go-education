4280a536e833bd10d1f1d12ee3fe493a2bc9487d
LeeDark <sergey.povarnin@gmail.com>
2022-05-04 15:30:53 +0300

    ServeMux HTTP Server: Added unit test TestPingClient.

--- trainee-tester/test-http-server/endpoints_test.go
+++ trainee-tester/test-http-server/endpoints_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+// this type is sufficient for testing HTTP responses
 func TestPing(t *testing.T) {
 	mux := setEndpoints()
 
@@ -18,5 +20,25 @@ func TestPing(t *testing.T) {
 	mux.ServeHTTP(w, req)
 	if w.Code != 200 {
 		t.Errorf("HTTP Status expected: 200, got: %d", w.Code)
+	}
+}
+
+// allows you to create an HTTP server for performing end-to-end HTTP tests
+// in which you can send HTTP requests to the server using an HTTP client
+func TestPingClient(t *testing.T) {
+	mux := setEndpoints()
+
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	usersUrl := fmt.Sprintf("%s/ping", server.URL)
+	request, _ := http.NewRequest("GET", usersUrl, nil)
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("HTTP Status expected: 200, got: %d", res.StatusCode)
 	}
 }
