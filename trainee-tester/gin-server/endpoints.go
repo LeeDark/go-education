d9bd912ae2af7860127e39d42e0084d6292a00aa
LeeDark <sergey.povarnin@gmail.com>
2022-05-10 01:45:43 +0300

    Gin HTTP Server: Minor fix.

--- trainee-tester/gin-server/endpoints.go
+++ trainee-tester/gin-server/endpoints.go
@@ -13,6 +13,7 @@ func setEndpoints() *gin.Engine {
 	// FIXME: don't use Logger middleware for benchmarking
 	e.Use(gin.Recovery())
 	e.LoadHTMLGlob("templates/*")
+
 	e.GET("/", home)
 	e.GET("/ping", ping)
 	return e
@@ -25,9 +26,5 @@ func home(c *gin.Context) {
 
 // Passive endpoint = HTTP Server gives JSON (XML) data = Frontend uses this JSON (XML) data
 func ping(c *gin.Context) {
-	//log.Println("Got ping")
-	// c.JSON(http.StatusOK, struct {
-	// 	Answer string `json:"answer"`
-	// }{Answer: "pong"})
 	c.JSON(http.StatusOK, gin.H{"answer": "pong"})
 }
