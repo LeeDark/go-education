5dc44bbfb59e7bbb4e5d52c8ceba5b924b3e37da
LeeDark <sergey.povarnin@gmail.com>
2022-05-19 17:48:10 +0300

    Init simple cdbSourceLogic. Added CdbSourceResponse.

--- trainee-tester/test-http-server/cdbsource.go
+++ trainee-tester/test-http-server/cdbsource.go
@@ -2,6 +2,33 @@ package main
 
 import "net/http"
 
+// CdbSourceResponse is struct for /cdbsource endpoint response
+type CdbSourceResponse struct {
+	Number string `json:"number"`
+	Mcc    string `json:"mcc"`
+	Mnc    string `json:"mnc"`
+	Ported bool   `json:"ported"`
+}
+
 func cdbSourceHandler(w http.ResponseWriter, req *http.Request) {
-	sendJSON(http.StatusOK, w, map[string]string{"answer": "pong"})
+	// request: /cdbsource?name=akton&number=123456789
+	name := req.URL.Query().Get("name")
+	number := req.URL.Query().Get("number")
+
+	// TODO: logic
+	response, err := cdbSourceLogic(name, number)
+	if err != nil {
+		sendJSON(http.StatusInternalServerError, w, nil)
+	}
+
+	// response: JSON {"number": "123456789", "mcc": MCC, "mnc": MNC, "ported": isPorted}
+
+	sendJSON(http.StatusOK, w, response)
+}
+
+func cdbSourceLogic(name, number string) (response *CdbSourceResponse, err error) {
+	response = &CdbSourceResponse{
+		Number: number,
+	}
+	return
 }
--- trainee-tester/test-http-server/endpoints.go
+++ trainee-tester/test-http-server/endpoints.go
@@ -22,9 +22,9 @@ func setEndpoints() *http.ServeMux {
 	return mux
 }
 
-func sendJSON(statisCode int, w http.ResponseWriter, data interface{}) error {
+func sendJSON(statusCode int, w http.ResponseWriter, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(statisCode)
+	w.WriteHeader(statusCode)
 	// TODO: add error handling
 	err := json.NewEncoder(w).Encode(&data)
 	if err != nil {
